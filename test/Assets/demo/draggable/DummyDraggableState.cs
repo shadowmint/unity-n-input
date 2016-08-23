d14970c0fcd930aea785acfb8c971435cb18bade
Douglas Linder <douglas.linder@gmail.com>
2016-08-23 19:23:16 +0800

    version

--- test/Assets/demo/draggable/DummyDraggableState.cs
+++ test/Assets/demo/draggable/DummyDraggableState.cs
@@ -1,69 +1,68 @@
 using UnityEngine;
 using N;
+using N.Package.Core;
 using N.Package.Input.Draggable;
 
 public class DummyDraggableState : MonoBehaviour
 {
-    public Color good;
-    public Color bad;
-    public Color idle;
-    public bool dragReady = true;
+  public Color good;
+  public Color bad;
+  public Color idle;
+  public bool dragReady = true;
 
-    private IDraggableSource source;
+  public void IsDragReady(DraggableEvent target)
+  {
+    target.accept = dragReady;
+  }
 
-    public void IsDragReady(DraggableEvent target)
+  public void OnStart(DraggableEvent ep)
+  {
+    var origin = ep.source.GameObject.AddComponent<Origin>();
+    origin.Bind();
+  }
+
+  public void OnStop(DraggableEvent ep)
+  {
+    var origin = ep.source.GameObject.GetComponent<Origin>();
+    ep.source.GameObject.Move(origin.position);
+    ep.source.GameObject.GetComponent<Renderer>().material.color = idle;
+    if (ep.source.DragCursor != null)
     {
-        target.accept = dragReady;
+      ep.source.DragCursor.GetComponent<Renderer>().material.color = idle;
     }
+  }
 
-    public void OnStart(DraggableEvent ep)
+  public void EnterTarget(DraggableEvent ep)
+  {
+    if (ep.accept)
     {
-        var origin = ep.source.GameObject.AddComponent<Origin>();
-        origin.Bind();
+      ep.source.GameObject.GetComponent<Renderer>().material.color = good;
+      if (ep.source.DragCursor != null)
+      {
+        ep.source.DragCursor.GetComponent<Renderer>().material.color = good;
+      }
     }
+    else
+    {
+      ep.source.GameObject.GetComponent<Renderer>().material.color = bad;
+      if (ep.source.DragCursor != null)
+      {
+        ep.source.DragCursor.GetComponent<Renderer>().material.color = bad;
+      }
+    }
+  }
 
-    public void OnStop(DraggableEvent ep)
+  public void LeaveTarget(DraggableEvent ep)
+  {
+    ep.source.GameObject.GetComponent<Renderer>().material.color = idle;
+    if (ep.source.DragCursor != null)
     {
-        var origin = ep.source.GameObject.GetComponent<Origin>();
-        ep.source.GameObject.Move(origin.position);
-        ep.source.GameObject.GetComponent<Renderer>().material.color = idle;
-        if (ep.source.DragCursor != null)
-        {
-            ep.source.DragCursor.GetComponent<Renderer>().material.color = idle;
-        }
+      ep.source.DragCursor.GetComponent<Renderer>().material.color = idle;
     }
+  }
 
-    public void EnterTarget(DraggableEvent ep)
-    {
-        if (ep.accept)
-        {
-            ep.source.GameObject.GetComponent<Renderer>().material.color = good;
-            if (ep.source.DragCursor != null)
-            {
-                ep.source.DragCursor.GetComponent<Renderer>().material.color = good;
-            }
-        }
-        else
-        {
-            ep.source.GameObject.GetComponent<Renderer>().material.color = bad;
-            if (ep.source.DragCursor != null)
-            {
-                ep.source.DragCursor.GetComponent<Renderer>().material.color = bad;
-            }
-        }
-    }
-
-    public void LeaveTarget(DraggableEvent ep)
-    {
-        ep.source.GameObject.GetComponent<Renderer>().material.color = idle;
-        if (ep.source.DragCursor != null)
-        {
-            ep.source.DragCursor.GetComponent<Renderer>().material.color = idle;
-        }
-    }
-
-    public void OnReceive(DraggableEvent rp)
-    {
-        GameObject.Destroy(rp.source.GameObject);
-    }
+  public void OnReceive(DraggableEvent rp)
+  {
+    GameObject.Destroy(rp.source.GameObject);
+  }
 }
